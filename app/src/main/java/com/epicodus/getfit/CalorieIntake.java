826eebfec4e459c3c7a5a5170587670e52d39999
lincoln <huylincoln007@yahoo.com>
2017-09-13 09:21:05 -0700

    refactoring code from main activity to calorie activity

--- app/src/main/java/com/epicodus/getfit/CalorieIntake.java
+++ app/src/main/java/com/epicodus/getfit/CalorieIntake.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -12,7 +13,9 @@ import butterknife.ButterKnife;
 
 public class CalorieIntake extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.userInputInformation) TextView mUserInputInformation;
-    @Bind(R.id.findExerciseList) Button mFindExerciseList;
+    @Bind(R.id.findDailyCalorieButton) Button mFindDailyCalorieButton;
+    @Bind(R.id.userAge) EditText mUserAge;
+    @Bind(R.id.userWeight) EditText mUserWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +23,24 @@ public class CalorieIntake extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_calorie_intake);
         ButterKnife.bind(this);
 
+        mFindDailyCalorieButton.setOnClickListener(this);
         Intent intent = getIntent();
         String age = intent.getStringExtra("age");
         String weight = intent.getStringExtra("weight");
         mUserInputInformation.setText("You enter " + age + " years and " + weight + " lbs. You're daily calorie intake based on your age and weight is 2000 calories");
 
-        mFindExerciseList.setOnClickListener(this);
+
 
     }
-            @Override
-            public void onClick(View view) {
-                Intent nagivateToExerciseList = new Intent(CalorieIntake.this, ExerciseProgram.class);
-                startActivity(nagivateToExerciseList);
-            }
-        }
+
+
+    @Override
+    public void onClick(View view) {
+        String age = mUserAge.getText().toString();
+        String weight = mUserWeight.getText().toString();
+        Intent intent = new Intent(this, CalorieIntake.class);
+        intent.putExtra("age", age);
+        intent.putExtra("weight", weight);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/epicodus/getfit/MainActivity.java
+++ app/src/main/java/com/epicodus/getfit/MainActivity.java
@@ -8,26 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @Bind(R.id.findDailyCalorieButton) Button mFindDailyCalorieButton;
-    @Bind(R.id.userAge) EditText mUserAge;
-    @Bind(R.id.userWeight) EditText mUserWeight;
-    @Bind(R.id.bottom_navigation) BottomNavigationView mBottomNavigationView;
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.bottom_navigation)
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mFindDailyCalorieButton.setOnClickListener(this);
 
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -50,16 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+}
 
-        @Override
-            public void onClick(View view) {
-                String age = mUserAge.getText().toString();
-                String weight = mUserWeight.getText().toString();
-                Intent intent = new Intent(MainActivity.this, CalorieIntake.class);
-                intent.putExtra("age", age);
-                intent.putExtra("weight", weight);
-                startActivity(intent);
-            }
-        }
 
 
