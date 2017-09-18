51009cfe8abd873f8f4c4d8d147deb38488627ee
lincoln <huylincoln007@yahoo.com>
2017-09-17 20:20:51 -0700

    attempt form validation

--- app/src/main/java/com/epicodus/getfit/CalorieIntake.java
+++ app/src/main/java/com/epicodus/getfit/CalorieIntake.java
@@ -3,6 +3,8 @@ package com.epicodus.getfit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -32,10 +34,44 @@ public class CalorieIntake extends AppCompatActivity implements View.OnClickList
         mFindDailyCalorieButton.setOnClickListener(this);
     }
 
+
+
     @Override
     public void onClick(View view) {
         Integer age = Integer.parseInt(mUserAge.getText().toString());
         Integer weight = Integer.parseInt(mUserWeight.getText().toString());
+
+        mUserAge.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if(s.length() == 0) {
+                    mUserAge.setError("Invalid Response");
+                }
+
+            }
+        });
+
+//        final String age1 = mUserAge.getText().toString();
+//        final String weight1 = mUserWeight.getText().toString();
+//
+//            if(mUserAge.getText().toString().length() == 0 ) {
+//                mUserAge.setError("Invalid Response");
+//                mUserAge.requestFocus();
+//            }
+
+//            if(mUserWeight.getText().toString().length() == 0 ) {
+//                mUserWeight.setError("Invalid Response");
+//                mUserWeight.requestFocus();
+//            }
 
         mUserInputInformation.setText("Based on your age and your current weight, your daily caloric intake is " +  calculateCalorie(age, weight) + " calories");
 
