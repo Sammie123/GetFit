d3cd010a53a5fd5c0ba1e6dc3f1e4f565c7269e5
lincoln <huylincoln007@yahoo.com>
2017-09-22 13:55:41 -0700

    add intents for login to create page

--- app/src/main/java/com/epicodus/getfit/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/getfit/CreateAccountActivity.java
@@ -1,13 +1,43 @@
 package com.epicodus.getfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (v == mCreateUserButton) {
+            createNewUser();
+        }
     }
 }
--- app/src/main/java/com/epicodus/getfit/LoginActivity.java
+++ app/src/main/java/com/epicodus/getfit/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.epicodus.getfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.registerTextView) TextView mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mRegister) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
